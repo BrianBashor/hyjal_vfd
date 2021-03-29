4797070248e82332633a5072d9d6cbccb37d36cd
Brian Bashor <brian.bashor@gmail.com>
2021-03-29 16:40:34 -0700

    changed to save imgaes locally

--- update_item.py
+++ update_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import os
-import time
 import urllib
 import pymongo
 from blizzard import Blizzard
 
 blizzard = Blizzard()
+
+IMG_PATH = "/Users/none/Documents/mongo/hyjal_vfd/item_media/"
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["hyjal"]
@@ -14,28 +15,22 @@ ah_collection = db["auction_house"]
 item_collection = db["items"]
 
 ah_item_id = ah_collection.distinct("item.id")
-item_id = item_collection.distinct("id")
+found_items = item_collection.distinct("id")
 
-n_of_item_to_insert = 0
-for ah_item in ah_item_id:
-    if ah_item not in item_id:
-        n_of_item_to_insert += 1
-
-for ah_item in ah_item_id:
-    if ah_item not in item_id:
-        print("Working on item: {}".format(ah_item))
-        insert_me = blizzard.game_item(ah_item)
-        item_media = insert_me["media"]["key"]["href"]
+item_collection.delete_many({})
+for item in ah_item_id:
+    if item not in found_items:
+        print("Working on item: {}".format(item))
+        asset = blizzard.game_item(item)
+        item_media = asset["media"]["key"]["href"]
         item_media = blizzard.generic_call(item_media)
         item_media = item_media["assets"][0]["value"]
 
-        urllib.request.urlretrieve(item_media, "/tmp/delete_me.jpg")
-        with open("/tmp/delete_me.jpg", 'rb') as img:
-            insert_me["media"] = img.read()
-        os.remove("/tmp/delete_me.jpg")
-        item_collection.insert_one(insert_me)
+        save_location = IMG_PATH + str(item) + ".jpg"
+        asset["media"] = save_location
+
+        urllib.request.urlretrieve(item_media, save_location)
         
-        print("{} Items remaining".format(n_of_item_to_insert))
-        n_of_item_to_insert -= 1
+        item_collection.insert_one(asset)
 
 print("Done!")
