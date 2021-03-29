b7b8ffda8c9537c689b9edbac968a7f4ba6792c6
Brian Bashor <brian.bashor@gmail.com>
2021-03-29 15:52:45 -0700

    write path to img insted of storing img in db

--- update_player_media.py
+++ update_player_media.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import os
-import time
 import urllib
 import pymongo
 from blizzard import Blizzard
 
 blizzard = Blizzard()
+
+IMG_PATH = "/Users/none/Documents/mongo/hyjal_vfd/player_media/"
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["hyjal"]
@@ -19,10 +19,10 @@ for player in db["guild_members"].find({}):
     assets = blizzard.profile_character_media(player["name"], player["server"])
     insert_me = {"name": player["name"]}
     for asset in assets["assets"]:
-        urllib.request.urlretrieve(asset["value"], "/tmp/delete_me.jpg")
-        with open("/tmp/delete_me.jpg", 'rb') as img:
-            insert_me[asset["key"]] = img.read()
-        os.remove("/tmp/delete_me.jpg")
+        save_location = IMG_PATH + player["name"] + "_" + asset["key"] + ".jpg"
+        urllib.request.urlretrieve(asset["value"], save_location)
+        insert_me[asset["key"]] = save_location
+
     collection.insert_one(insert_me)
 
 print("Done!")
