36f63c1e1a1139b5bc0531467fe8058f195ed4ad
Brian Bashor <brian.bashor@gmail.com>
2021-03-29 11:07:32 -0700

    none

--- update_m_pluse_leaderboard.py
+++ update_m_pluse_leaderboard.py
@@ -5,37 +5,38 @@ from blizzard import Blizzard
 
 blizzard = Blizzard()
 
-dungeon_indexes = blizzard.game_mythic_keystone_dungeons_index()
-season_indexes = blizzard.game_mythic_keystone_season_index()
-season_index = season_indexes["current_season"]["key"]["href"]
-current_season_info = blizzard.generic_call(season_index)
-current_season_num = season_indexes["current_season"]["id"]
+dungeon_indexes = blizzard.game_mythic_keystone_dungeons_index()["dungeons"]
+current_season = blizzard.game_mythic_keystone_season_index()["current_season"]
+season_periods = blizzard.generic_call(current_season["key"]["href"])
+season_periods = season_periods["periods"]
+current_season = current_season["id"]
+current_period = season_periods[-1]["id"]
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["hyjal"]
-collection = db["m_plus_season_" + str(current_season_num)]
+collection = db["m_plus_season_" + str(current_season)]
 
-cursor = collection.find({})
-last_period_update = 0
-for doc in cursor: 
-    if doc["period"] > last_period_update:
-        last_period_update = doc["period"]
+if collection.count_documents({}) == 0:
+    start_period = season_periods[0]["id"]
+else:
+    start_period = collection.find_one(sort=[("period", pymongo.DESCENDING)])
+    start_period = start_period["period"]
 
-current_period = 0
-for current_season in current_season_info["periods"]:
-    if current_season["id"] > current_period:
-        current_period = current_season["id"]
-
-for period in range(last_period_update, current_period + 1):
-    for dungeon_index in dungeon_indexes["dungeons"]:
-        search_me = {
-            "period": period, 
-            "map_challenge_mode_id": dungeon_index["id"]
-        }
-        insert_me = blizzard.game_mythic_keystone_leaderboard(dungeon_index["id"], period)
-        if period == last_period_update: 
-            collection.find_one_and_replace(search_me, insert_me)
-        else: 
-            collection.insert_one(insert_me)
+for period in range(start_period, current_period + 1):
+    for dungeon_index in dungeon_indexes:
+        insert_me = blizzard.game_mythic_keystone_leaderboard(dungeon_index["id"], period) 
+        if collection.find_one({"period": period, "map_challenge_mode_id": dungeon_index["id"]}):
+            print("Replacing for peroid {} : {}".format( period, dungeon_index["name"],))
+            collection.replace_one({
+                "period": period,
+                "map_challenge_mode_id": dungeon_index
+                }, insert_me)
+        else:
+            try:
+                if insert_me["leading_groups"]:
+                    print("Writing for peroid {} : {}".format(period, dungeon_index["name"],))
+                    collection.insert_one(insert_me)
+            except Exception as e:
+                print("No data for peroid {} : {}".format(period, dungeon_index["name"]))
 
 print("Done!")
