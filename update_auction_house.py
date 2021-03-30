71f0b7102cb700cd738080c3c81f0886d0d35a63
Brian Bashor <brian.bashor@gmail.com>
2021-03-30 12:47:14 -0700

    added insert date.

--- update_auction_house.py
+++ update_auction_house.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pymongo
+from datetime import datetime
 from blizzard import Blizzard
 
 blizzard = Blizzard()
@@ -10,7 +11,11 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["hyjal"]
 collection = db["auction_house"]
 
+now = datetime.now()
+now = now.strftime("%Y_%m_%d_%H_%M_%S")
+
 for item in ah_data["auctions"]:
+    item["date_time"] = now
     collection.insert_one(item)
 
 print("Done!")
