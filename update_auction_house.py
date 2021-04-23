6e6fdfb1ee5ba36458ededc3d6e8bb7f3a6df12c
Brian Bashor <brian.bashor@gmail.com>
2021-04-23 09:21:55 -0700

    none

--- update_auction_house.py
+++ update_auction_house.py
@@ -8,29 +8,35 @@ from blizzard import Blizzard
 
 blizzard = Blizzard()
 
-IMG_PATH = "/mnt/disk1/item_media/"
+IMG_PATH = "/mnt/ext0/media/item/"
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["hyjal"]
-collection = db["auction_house"]
-collection_ah = db["auction_house"].distinct("item.id")
-collection_id = db["items"].distinct("id")
 
 ah_data = blizzard.auction_house()
+current_time = datetime.utcnow()
 
 for item in ah_data["auctions"]:
-    collection.insert_one(item)
+    item["date_time"] = current_time
+    db["auction_house"].insert_one(item)
 
-for item in db["auction_house"].distinct("item.id"):
-    if item not in collection_id:
+my_ah = db["auction_house"].distinct("item.id")
+my_items = db["items"].distinct("id")
+diff = len(my_ah) - len(my_items) -1
+
+print("{} total new items to insert".format(diff))
+for item in my_ah:
+    if item not in my_items:
         asset = blizzard.game_item(item)
         if asset is not None:
+            print("Inserting item: {} : {} remaining".format(item, diff))
             item_media = asset["media"]["key"]["href"]
             item_media = blizzard.generic_call(item_media)
             item_media = item_media["assets"][0]["value"]
-
             full_file_path = IMG_PATH + str(item) + ".jpg"
             asset["media"] = full_file_path
             urllib.request.urlretrieve(item_media, full_file_path)
             db["items"].insert_one(asset)
-            time.sleep(1.1)
+            time.sleep(1)
+        else: print("Can't find {}".format(item))
+        diff -= 1
