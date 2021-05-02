a11dc168634e0978a7ab496cc8595066affaece6
Brian Bashor <brian.bashor@gmail.com>
2021-05-02 08:51:14 +0000

    stuff goes here

--- auction_house.py
+++ auction_house.py
@@ -1,39 +1,49 @@
 #!/usr/bin/env python3
 
 import time
-import pymongo
 import urllib
+import pymongo
+import threading
 from datetime import datetime
 from blizzard import Blizzard
 
 blizzard = Blizzard()
 
-IMG_PATH = "/mnt/ext0/media/item/"
+current_time = datetime.utcnow()
+IMG_PATH = '/mnt/disk1/media/item/'
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["hyjal"]
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['hyjal']
+
+def get_item(item_n: str) -> None:
+    item = blizzard.game_item(item_n)
+    if item is not None:
+        item_media = item['media']['key']['href']
+        item_media = blizzard.generic_call(item_media)
+        item_media = item_media['assets'][0]['value']
+        full_file_path = IMG_PATH + str(item_n) + '.jpg'
+        item['media'] = full_file_path
+        urllib.request.urlretrieve(item_media, full_file_path)
+        db['items'].insert_one(item)
 
 ah_data = blizzard.auction_house()
-current_time = datetime.utcnow()
+if ah_data is not None:
+    for item in ah_data['auctions']:
+        item['date_time'] = current_time
+        db['auction_house'].insert_one(item)
 
-for item in ah_data["auctions"]:
-    item["date_time"] = current_time
-    db["auction_house"].insert_one(item)
+new_items = []
+for item in db['auction_house'].distinct('item.id'):
+    if item not in db['items'].distinct('id'):
+        new_items.append(str(item))
 
-my_ah = db["auction_house"].distinct("item.id")
-my_items = db["items"].distinct("id")
-
-for item in my_ah:
-    if item not in my_items:
-        asset = blizzard.game_item(item)
-        if asset is not None:
-            print("Inserting item: {} : {} remaining".format(item, diff))
-            item_media = asset["media"]["key"]["href"]
-            item_media = blizzard.generic_call(item_media)
-            item_media = item_media["assets"][0]["value"]
-            full_file_path = IMG_PATH + str(item) + ".jpg"
-            asset["media"] = full_file_path
-            urllib.request.urlretrieve(item_media, full_file_path)
-            db["items"].insert_one(asset)
-            time.sleep(1)
-        diff -= 1
+for _ in range(10):
+    threads = []
+    for _ in range(50):
+        item_n = str(new_items.pop())
+        t = threading.Thread(target=get_item, args=[item_n])
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
+    time.sleep(3)
--- blizzard.py
+++ blizzard.py
@@ -12,93 +12,92 @@ class Blizzard:
         self.access_token = json.loads(r.text)['access_token']
         
     def send_it(self, r):
+        self.api_count += 1
         if r.status_code == 200:
             return json.loads(r.text)
         else:
-            print("ERROR: {}".format(r.status_code))
+            print('ERROR: {}'.format(r.status_code))
             return None
 
     # Game API Call #
-    def game_mythic_keystone_season_index(self, n=5):
-        r = requests.get("https://us.api.blizzard.com/data/wow/mythic-keystone/season/index?namespace=dynamic-us&locale=en_US&access_token={}".format(self.access_token))
-        if r.status_code != 200 and n > 0:
-            self.game_mythic_keystone_season_index(n - 1)
+    def game_mythic_keystone_season_index(self):
+        r = requests.get('https://us.api.blizzard.com/data/wow/mythic-keystone/season/index?namespace=dynamic-us&locale=en_US&access_token={}'.format(self.access_token))
         return self.send_it(r)
 
     def game_professions_index(self):
-        r = requests.get("https://us.api.blizzard.com/data/wow/profession/index?namespace=static-us&locale=en_US&access_token={}".format(self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/profession/index?namespace=static-us&locale=en_US&access_token={}'.format(self.access_token))
         return self.send_it(r)
 
     def game_profession(self, n):
-        r = requests.get("https://us.api.blizzard.com/data/wow/profession/{}?namespace=static-us&locale=en_US&access_token={}".format(str(n), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/profession/{}?namespace=static-us&locale=en_US&access_token={}'.format(str(n), self.access_token))
         return self.send_it(r)
 
     def game_mythic_keystone_dungeons_index(self):
-        r = requests.get("https://us.api.blizzard.com/data/wow/mythic-keystone/dungeon/index?namespace=dynamic-us&locale=en_US&access_token={}".format(self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/mythic-keystone/dungeon/index?namespace=dynamic-us&locale=en_US&access_token={}'.format(self.access_token))
         return self.send_it(r)
 
     def game_mythic_keystone_dungeons(self, index):
-        r = requests.get("https://us.api.blizzard.com/data/wow/mythic-keystone/dungeon/{}?namespace=dynamic-us&locale=en_US&access_token={}".format(str(index), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/mythic-keystone/dungeon/{}?namespace=dynamic-us&locale=en_US&access_token={}'.format(str(index), self.access_token))
         return self.send_it(r)
 
     def game_mythic_keystone_period_index(self):
-        r = requests.get("https://us.api.blizzard.com/data/wow/mythic-keystone/period/index?namespace=dynamic-us&locale=en_US&access_token={}".format(self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/mythic-keystone/period/index?namespace=dynamic-us&locale=en_US&access_token={}'.format(self.access_token))
         return self.send_it(r)
 
     def game_mythic_keystone_leaderboard(self, dungeon_id, period, cr_id=1426):
-        r = requests.get("https://us.api.blizzard.com/data/wow/connected-realm/{}/mythic-leaderboard/{}/period/{}?namespace=dynamic-us&locale=en_US&access_token={}".format(str(cr_id), str(dungeon_id), str(period), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/connected-realm/{}/mythic-leaderboard/{}/period/{}?namespace=dynamic-us&locale=en_US&access_token={}'.format(str(cr_id), str(dungeon_id), str(period), self.access_token))
         return self.send_it(r)
 
     def game_playable_class_index(self):
-        r = requests.get("https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-us&locale=en_US&access_token={}".format(self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-us&locale=en_US&access_token={}'.format(self.access_token))
         return self.send_it(r)
 
     def game_playable_class(self, class_id):
-        r = requests.get("https://us.api.blizzard.com/data/wow/playable-class/{}?namespace=static-us&locale=en_US&access_token={}".format(str(class_id), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/playable-class/{}?namespace=static-us&locale=en_US&access_token={}'.format(str(class_id), self.access_token))
         return self.send_it(r)
 
     def game_playable_class_media(self, class_id):
-        r = requests.get("https://us.api.blizzard.com/data/wow/media/playable-class/{}?namespace=static-us&locale=en_US&access_token={}".format(str(class_id), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/media/playable-class/{}?namespace=static-us&locale=en_US&access_token={}'.format(str(class_id), self.access_token))
         return self.send_it(r)
 
     def game_playable_specialization(self, id):
-        r = requests.get("https://us.api.blizzard.com/data/wow/playable-specialization/{}?namespace=static-us&locale=en_US&access_token={}".format(str(id), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/playable-specialization/{}?namespace=static-us&locale=en_US&access_token={}'.format(str(id), self.access_token))
         return self.send_it(r)
 
     def game_playable_specialization_index(self):
-        r = requests.get("https://us.api.blizzard.com/data/wow/playable-specialization/index?namespace=static-us&locale=en_US&access_token={}".format(self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/playable-specialization/index?namespace=static-us&locale=en_US&access_token={}'.format(self.access_token))
         return self.send_it(r)
 
     def game_mythic_keystone_seasons(self):
-        r = requests.get("https://us.api.blizzard.com/data/wow/mythic-keystone/season/index?namespace=dynamic-us&locale=en_US&access_token={}".format(self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/mythic-keystone/season/index?namespace=dynamic-us&locale=en_US&access_token={}'.format(self.access_token))
         return self.send_it(r)
 
     def game_item(self, item_id):
-        r = requests.get("https://us.api.blizzard.com/data/wow/item/{}?namespace=static-us&locale=en_US&access_token={}".format(str(item_id), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/item/{}?namespace=static-us&locale=en_US&access_token={}'.format(str(item_id), self.access_token))
         return self.send_it(r)
 
     def game_item_media(self, item_id):
-        r = requests.get("https://us.api.blizzard.com/data/wow/media/item/{}?namespace=static-us&locale=en_US&access_token={}".format(str(item_id), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/media/item/{}?namespace=static-us&locale=en_US&access_token={}'.format(str(item_id), self.access_token))
         return self.send_it(r)
 
     def profile_character_equipment(self, name, server):
-        r = requests.get("https://us.api.blizzard.com/profile/wow/character/{}/{}/equipment?namespace=profile-us&locale=en_US&access_token={}".format(server, name, self.access_token))
+        r = requests.get('https://us.api.blizzard.com/profile/wow/character/{}/{}/equipment?namespace=profile-us&locale=en_US&access_token={}'.format(server, name, self.access_token))
         return self.send_it(r)
 
     def profile_character_profession(self, name, server):
-        r = requests.get("https://us.api.blizzard.com/profile/wow/character/{}/{}/professions?namespace=profile-us&locale=en_US&access_token={}".format(server, name, self.access_token))
+        r = requests.get('https://us.api.blizzard.com/profile/wow/character/{}/{}/professions?namespace=profile-us&locale=en_US&access_token={}'.format(server, name, self.access_token))
         return self.send_it(r)
 
     def profile_character_profile(self, name, server, n=5):
-        r = requests.get("https://us.api.blizzard.com/profile/wow/character/{}/{}?namespace=profile-us&locale=en_US&access_token={}".format(server, name, self.access_token))
+        r = requests.get('https://us.api.blizzard.com/profile/wow/character/{}/{}?namespace=profile-us&locale=en_US&access_token={}'.format(server, name, self.access_token))
         return self.send_it(r)
 
     def profile_character_pvp(self, name, server, bracket):
-        r = requests.get("https://us.api.blizzard.com/profile/wow/character/{}/{}/pvp-bracket/{}?namespace=profile-us&locale=en_US&access_token={}".format(server, name, bracket, self.access_token))
+        r = requests.get('https://us.api.blizzard.com/profile/wow/character/{}/{}/pvp-bracket/{}?namespace=profile-us&locale=en_US&access_token={}'.format(server, name, bracket, self.access_token))
         return self.send_it(r)
 
     def profile_character_media(self, name, server):
-        r = requests.get("https://us.api.blizzard.com/profile/wow/character/{}/{}/character-media?namespace=profile-us&locale=en_US&access_token={}".format(server, name, self.access_token))
+        r = requests.get('https://us.api.blizzard.com/profile/wow/character/{}/{}/character-media?namespace=profile-us&locale=en_US&access_token={}'.format(server, name, self.access_token))
         return self.send_it(r)
 
     def profile_specialization(self, url):
@@ -106,10 +105,10 @@ class Blizzard:
         return self.send_it(r)
 
     def generic_call(self, url):
-        r = requests.get(url + "&access_token=" + self.access_token)
+        r = requests.get(url + '&access_token=' + self.access_token)
         return self.send_it(r)
 
     def auction_house(self, cr_id=1426):
-        r = requests.get("https://us.api.blizzard.com/data/wow/connected-realm/{}/auctions?namespace=dynamic-us&locale=en_US&access_token={}".format(str(cr_id), self.access_token))
+        r = requests.get('https://us.api.blizzard.com/data/wow/connected-realm/{}/auctions?namespace=dynamic-us&locale=en_US&access_token={}'.format(str(cr_id), self.access_token))
         return self.send_it(r)
 
